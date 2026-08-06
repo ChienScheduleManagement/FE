81fb5c34ce64202bd0c9fa81ad99855dfbde6c94
Đàm Minh Chiến <damminhchien220204@gmail.com>
2026-08-06 20:47:59 +0700

    feat(FE): update default pageSize for tables to 20

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -79,7 +79,7 @@ export function DataTable<TData, TValue>({
   const [internalSorting, setInternalSorting] = useState<SortingState>([])
   const [internalPagination, setInternalPagination] = useState<PaginationState>({
     pageIndex: 0,
-    pageSize: 10,
+    pageSize: 20,
   })
   const [internalGlobalFilter, setInternalGlobalFilter] = useState('')
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
